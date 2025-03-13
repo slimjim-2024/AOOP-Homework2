f066dc98f65d1fb112ae0bbc7649dd700d244cc9
Dalius69 <dalius11d@gmail.com>
2025-03-13 11:28:13 +0100

    created login page

---
+++ Login.axaml.cs
@@ -0,0 +1,72 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Tmds.DBus.Protocol;
+
+namespace AOOP_Homework2;
+
+public partial class Login : Window
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginButton_Click(object sender, RoutedEventArgs e)
+    {
+
+        // var loginViewModel = (LoginViewModel)DataContext;
+        
+        // if (loginViewModel.Login())
+        // {
+        //     MessageBox.Show("Login successful!");
+        // }
+        // else
+        // {
+        //     MessageBox.Show("Login failed!");
+        // }
+        Console.WriteLine("Login button clicked! Username: {0}, Password: {1}", Username.Text, Password.Text);
+    }private void TeacherLoginButton_Click(object sender, RoutedEventArgs e)
+    {
+        // var loginViewModel = (LoginViewModel)DataContext;
+        
+        // if (loginViewModel.Login())
+        // {
+        //     MessageBox.Show("Login successful!");
+        // }
+        // else
+        // {
+        //     MessageBox.Show("Login failed!");
+        // }
+        Console.WriteLine("Login button clicked! Username: {0}, Password: {1}", TeacherUsername.Text, TeacherPassword.Text);
+    }
+
+    private void RegisterButton_Click(object sender, RoutedEventArgs e)
+    {
+        // var loginViewModel = (LoginViewModel)DataContext;
+        
+        // if (loginViewModel.Register())
+        // {
+        //     MessageBox.Show("Registration successful!");
+        // }
+        // else
+        // {
+        //     MessageBox.Show("Registration failed!");
+        // }
+    }
+    private void TeacherRegisterButton_Click(object sender, RoutedEventArgs e)
+    {
+        // var loginViewModel = (LoginViewModel)DataContext;
+        
+        // if (loginViewModel.Register())
+        // {
+        //     MessageBox.Show("Registration successful!");
+        // }
+        // else
+        // {
+        //     MessageBox.Show("Registration failed!");
+        // }
+    }
+}
---
+++ LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AOOP_Homework2;
+
+public partial class LoginViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _username = "";
+    
+    [ObservableProperty]
+    private string _password = "";
+}
